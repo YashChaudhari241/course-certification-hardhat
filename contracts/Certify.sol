c1fed3c530e7c89ac7bde5235beb6a08314cc976
Yash Chaudhari <yash_chaudhari@live.com>
2023-04-18 03:03:24 +0530

    small fixes

--- contracts/Certify.sol
+++ contracts/Certify.sol
@@ -6,11 +6,12 @@ pragma solidity ^0.8.9;
 
 contract Certify {
     address immutable i_owner;
-    mapping (address => uint8) public authorities; // used to store authorities
-    mapping (address => uint8) public issuers;// used to store authorities
-    mapping (address => Certificate[]) certificates;
+    mapping(address => uint8) public authorities; // used to store authorities
+    mapping(address => uint8) public issuers; // used to store authorities
+    mapping(address => Certificate[]) certificates;
     error Certify__OutOfIndex();
-    struct Certificate{
+    struct Certificate {
+        string id;
         address issuedTo;
         address issuedBy;
         uint issuedAt;
@@ -22,49 +23,72 @@ contract Certify {
         i_owner = owner;
         authorities[owner] = 1;
     }
-    modifier onlyAuthorities {
-        require(authorities[msg.sender] ==1 || msg.sender == i_owner, "Not an Authority");
+
+    modifier onlyAuthorities() {
+        require(
+            authorities[msg.sender] == 1 || msg.sender == i_owner,
+            "Not an Authority"
+        );
         _;
     }
-    modifier onlyOwner {
+    modifier onlyOwner() {
         require(msg.sender == i_owner, "Only owners can call this function");
         _;
     }
-    modifier onlyIssuer {
-        require(issuers[msg.sender] ==1, "Only issuers can call this function");
+    modifier onlyIssuer() {
+        require(
+            issuers[msg.sender] == 1,
+            "Only issuers can call this function"
+        );
         _;
     }
-    function addAuthority (address newAuthority) external onlyAuthorities {
+
+    function addAuthority(address newAuthority) external onlyAuthorities {
         authorities[newAuthority] = 1;
     }
-    function removeAuthority(address existingAuthority) external onlyOwner{
+
+    function removeAuthority(address existingAuthority) external onlyOwner {
         authorities[existingAuthority] = 0;
     }
+
     function addIssuer(address newIssuer) external onlyAuthorities {
         issuers[newIssuer] = 1;
     }
+
     function removeIssuer(address existingIssuer) external onlyAuthorities {
         issuers[existingIssuer] = 0;
     }
-    function issueCertificate(Certificate memory newCert) external onlyIssuer{
+
+    function issueCertificate(Certificate memory newCert) external onlyIssuer {
         newCert.issuedAt = block.timestamp;
         newCert.issuedBy = msg.sender;
         certificates[newCert.issuedTo].push(newCert);
     }
-    function revokeCertificate(address user, uint index) external onlyIssuer{
-        if(certificates[user].length <= index){
+
+    function revokeCertificate(address user, uint index) external onlyIssuer {
+        if (certificates[user].length <= index) {
             revert Certify__OutOfIndex();
         }
         certificates[user][index].isRevoked = true;
     }
-    function getAllCertificates(address user) external view returns (Certificate[] memory){
+
+    function getAuthorityStatus() external view returns (uint) {
+        return authorities[msg.sender];
+    }
+
+    function getAllCertificates(
+        address user
+    ) external view returns (Certificate[] memory) {
         return certificates[user];
     }
-    function getCertificate(address user,uint index) external view returns (Certificate memory){
-        if(certificates[user].length <= index){
+
+    function getCertificate(
+        address user,
+        uint index
+    ) external view returns (Certificate memory) {
+        if (certificates[user].length <= index) {
             revert Certify__OutOfIndex();
         }
         return certificates[user][index];
     }
-
 }
